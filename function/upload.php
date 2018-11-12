da2870532b174eebec2683b23d6fda6a025ef48a
WIJTB <104023005@live.asia.edu.tw>
2018-11-12 22:49:31 +0800

    fix code

--- function/upload.php
+++ function/upload.php
@@ -1,26 +1,23 @@
 <?php
 # 取得上傳檔案數量
 $fileCount = count($_FILES['my_file']['name']);
-
 for ($i = 0; $i < $fileCount; $i++) {
-    # 檢查檔案是否上傳成功
-    if ($_FILES['my_file']['error'][$i] === UPLOAD_ERR_OK){
+
+    if ($_FILES['my_file']['error'][$i] === UPLOAD_ERR_OK) # 檢查檔案是否上傳成功
+    {
         echo '檔案名稱: ' . $_FILES['my_file']['name'][$i] . '<br/>';
         echo '檔案類型: ' . $_FILES['my_file']['type'][$i] . '<br/>';
-        echo '檔案大小: ' . ($_FILES['my_file']['size'][$i] / 1024) . ' KB<br/>';
-        echo '暫存名稱: ' . $_FILES['my_file']['tmp_name'][$i] . '<br/>';
-
-        # 檢查檔案是否已經存在
-        if (file_exists('../upload/' . $_FILES['my_file']['name'][$i]))
-        {
-            echo '檔案已存在。<br/>';
-        }
-        else {
-            $file = $_FILES['my_file']['tmp_name'][$i];
-            $dest = '../upload/' . $_FILES['my_file']['name'][$i];
-            # 將檔案移至指定位置
-            move_uploaded_file($file, $dest);
-        }
+        echo '檔案大小: ' . sprintf("%.2f",($_FILES['my_file']['size'][$i] / (1024*1024))). ' MB<br/>';
+            if (file_exists('../upload/' . $_FILES['my_file']['name'][$i]))  # 檢查檔案是否已經存在
+            {
+                echo '檔案已存在。<br/>';
+            }
+            else {
+                $file = $_FILES['my_file']['tmp_name'][$i];
+                $dest = '../upload/' . $_FILES['my_file']['name'][$i];
+                # 將檔案移至指定位置
+                move_uploaded_file($file, $dest);
+            }
        echo  '<script> alert("檔案上傳成功"); </script>';
     }
     else {
