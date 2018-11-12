760f5ea59c0abd96c6f20ead11e51fce4cb22401
mimi870109 <43132590+mimi870109@users.noreply.github.com>
2018-11-12 17:40:30 +0800

    Update buy.php

---
+++ buy.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GoPor影片集合地</title>
+    <link rel="stylesheet" href="css/home_page.css">
+</head>
+<body >
+<h1 align="center" class="qqq">Gopro影片集中地 </h1>
+
+<ul class="drop-down-menu">
+    <li><a href="">關於我們</a>
+        <ul>
+            <li><a href="">創作動機</a></li>
+            <li><a href="">拍攝器材</a></li>
+            <li><a href="">聯絡方式</a></li>
+        </ul>
+    </li>
+
+    <li><a href="">我的作品</a>
+        <ul>
+            <li><a href="">經典作品</a></li>
+            <li><a href="">素材</a></li>
+        </ul>
+    </li>
+
+    <li><a href="">教學專案</a>
+        <ul>
+            <li><a href="">費用計算</a></li>
+            <li><a href="">準備器材</a></li>
+        </ul>
+    <li><a href="">購買方案</a>
+        <ul>
+            <li><a href="">費率計算</a></li>
+            <li><a href="">年費申請</a></li>
+        </ul>
+    </li>
+    <li><a href="">我要貢獻影片</a></li>
+    <li><a href="">登入</a></li>
+</ul>
+<?php
+?>
