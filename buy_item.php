6be4e101d98c3af44d38317f25db96b7eb0233f8
mimi870109 <43132590+mimi870109@users.noreply.github.com>
2018-11-12 21:10:18 +0800

    購買業面初製作

--- buy_item.php
+++ buy_item.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <title>GoPore購買影片</title>
     <link rel="stylesheet" href="css/home_page.css">
+    <link rel="stylesheet" href="css/buy_item.css">
 </head>
 <body >
 <h1 align="center" class="qqq">Gopro影片集中地 </h1>
 
 <ul class="drop-down-menu">
-    <li><a href="">關於我們</a>
+    <li><a href="game.html">關於我們</a>
         <ul>
             <li><a href="">創作動機</a></li>
             <li><a href="">拍攝器材</a></li>
@@ -38,5 +39,14 @@
     <li><a href="">我要貢獻影片</a></li>
     <li><a href="">登入</a></li>
 </ul>
+<hr>
+<div class="content">
+    <div class="video_buy"> <video src="video/111.MP4" loop autoplay="autoplay" muted controls="controls"  height="100%"></video></div>
+    <div class="button"> <button value="購買">購買</button></div>
+</div>
+<hr>
+<div class="content">
+    <div class="video_buy"> <video src="video/116.MP4" loop autoplay="autoplay" muted controls="controls"  height="100%">111</video></div>
+</div>
 <?php
 ?>
