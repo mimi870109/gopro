f460f5a0a00a40fc9ea6f6fb27d813d4dbc3a07a
WIJTB <104023005@live.asia.edu.tw>
2018-11-12 21:57:08 +0800

    fix button code

--- sell_item.php
+++ sell_item.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8">
     <title>販賣影片</title>
@@ -8,6 +8,11 @@
 
 ?>
 <body>
-
+<form>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        檔案名稱:<input type="file" name="file" id="file" /><br />
+        <input type="submit" name="submit" value="上傳檔案" />
+    </form>
+</form>
 </body>
 </html>
---
+++ upload.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WIJTB
+ * Date: 2018/11/12
+ * Time: PM 05:59
+ */
