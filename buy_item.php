39800c62bfe130c72b827c913e5508930fafa2b6
WIJTB <104023005@live.asia.edu.tw>
2018-11-15 22:35:07 +0800

    修改導航˙列

--- buy_item.php
+++ buy_item.php
@@ -7,10 +7,11 @@
     <link rel="stylesheet" href="css/buy_item.css">
 </head>
 <body>
-<div class="header"><h1 align="center" class="qqq">Gopro影片集中地 </h1></div>
-
-
-<ul class="drop-down-menu" >
+<div class="header">
+	<h1>Gopro影片集中地</h1>
+</div>
+	<nav class="Nav_Bar">
+		<ul class="drop-down-menu" >
     <li><a href="game.html">關於我們</a>
         <ul>
             <li><a href="">創作動機</a></li>
@@ -40,10 +41,11 @@
     <li><a href="">我要貢獻影片</a></li>
     <li><a href="./Login_v15/index.html">登入</a></li>
 </ul>
+	</nav>
 <hr>
 <div class="content">
     <div class="video_buy"> 
-		<video src="video/111.MP4" loop autoplay="autoplay" muted controls="controls"  height="100%"></video>
+		<video src="video/111.MP4" loop controls muted controls="controls"  height="100%"></video>
 	</div>
     <div class="text_buy">
 		<p>名稱:海浪之星</p>
@@ -51,11 +53,11 @@
 		<p>作者:mimi870109</p>
 		<p>關於:衝浪的我怎麼那麼帥</p>
 	</div>
-    <div class="test"><a href="">立即購買</a></div>
+    <div class="buy_button"><a href="">立即購買</a></div>
 </div>
 <hr>
 <div class="content">
-    <div class="video_buy"> <video src="video/116.MP4" loop autoplay="autoplay" muted controls="controls"  height="100%">111</video></div>
+    <div class="video_buy"> <video src="video/116.MP4" loop  muted controls="controls"  height="100%">111</video></div>
 </div>
 
 
