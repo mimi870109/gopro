cf0705b104cbfbfcb4cf6bb38c64db1e2838e5b3
WIJTB <104023005@live.asia.edu.tw>
2018-12-07 23:54:11 +0800

    增加審核頁面美觀

--- check_file.php
+++ check_file.php
@@ -1,22 +1,56 @@
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/html">
 <head>
-    <script>
-        window.alert()
-    </script>
-    <?php
-    include ('./login/Mysql_check.php');
-    ?>
-    <meta charset="utf-8">
-    <title>販賣影片</title>
+<?php
+	include ('./login/Mysql_check.php');
+?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>販賣影片</title>
     <style>
         #Upload_Form > *{
             margin-top: 10px;
         }
+		.container{
+			background-color: white;
+			width: 70%;
+			height: 335px;
+			padding: 1px;
+			margin-top: 5px;
+		}
+		.Input_Group{
+			margin: 5px 0px;
+			position: relative;
+			float: left;
+			width: 50%;
+			height: 98px;
+		}
+		.Check_Bt_Group{
+			position: relative;
+			float: right;
+			margin: 130px 60px;
+			width: 20%;
+			height: 98px;
+		}
+		.Bt_Box{
+			height: 100%;
+			width: 100%;
+		}
+		.Bt_Box > *{
+			margin: 8px 0px;
+			width: 100%;
+		}
+		.img-responsive{
+			margin: 3px auto;
+			max-width: 274px;
+			max-height: 180px;
+		}
     </style>
     <link rel="stylesheet" href="css/home_page.css">
+	<link rel="stylesheet" href="css/bootstrap-3.3.7.css">
 </head>
-<body>
+<body style="background-color: #808c92; overflow-x: hidden;">
 <div class="header">
     <h1>Gopro影片集中地</h1>
 </div>
@@ -51,25 +85,46 @@
         <li><a href="./login/login.php">登入</a></li>
     </ul>
 </nav>
-<div>須審核列表</div>
+<div style="margin: 30px 53% 10px 47%; color: white; width: 100%; height: 30px; align-content: center"; ><h3>須審核列表</h3></div>
 <?php
 $Search_Data = "SELECT * FROM `check_sell_item`";
 $result = $link->query($Search_Data);
 if ($result->num_rows > 0) { //判斷是否超過1條數據
         while ($row = $result->fetch_assoc()) // 輸出數據
         {
-            echo $row['email']."<br>";
-            echo $row['title']."<br>";
-            echo $row['description']."<br>";
             ?>
-            <img src="<?php echo $row['file_path']; ?>"  height="100" width="100">
+				<div class="container">
+					<div class="Input_Group">
+						<div class="input-group">
+							<span id="addon1" class="input-group-addon">使用者</span>
+							<input type="text" class="form-control" placeholder="normal sized input group" value="<?php echo $row['email']; ?>">
+						</div>
+						<div class="input-group">
+							<span class="input-group-addon">標題</span>
+							<input type="text" class="form-control" placeholder="placeholder content" value="<?php echo $row['title']; ?>">
+						</div>
+						<div class="input-group">
+							<span class="input-group-addon">介紹</span>
+							<input type="text" class="form-control" placeholder="placeholder content" value="<?php echo $row['description']; ?>">
+						</div>
+							<img src="<?php echo $row['file_path']; ?>" class="img-responsive" alt="Placeholder image">
+						<div class="input-group">
+							<span class="input-group-addon">價格 NTD$</span>
+							<input type="text" class="form-control" placeholder="placeholder content" value="<?php echo $row['price']; ?>">
+						</div>
+					</div>
+					<div class="Check_Bt_Group">
+						<div class="Bt_Box">
+							<button type="button" class="btn btn-default">確認</button>
+							<button type="button" class="btn btn-default">拒絕</button>
+						</div>
+					</div>
+				</div>
 <?php
-
-            echo $row['price']."<br>";
         }
 }
 else{
-    echo "沒有需要審核的影片"."<br>";
+    echo "<div style=\"margin: 30px 56% 10px 44%; color: white; width: 100%; height: 30px; align-content: center\"; ><h3>沒有需要審核的影片</h3></div>";
 }
 ?>
 </body>
