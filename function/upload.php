0e5c912ad2c9944bf3b52b812020ce6323b0f29d
WIJTB <104023005@live.asia.edu.tw>
2018-12-13 15:44:55 +0800

    fix code

--- function/upload.php
+++ function/upload.php
@@ -26,35 +26,38 @@ else{
         $Fasle_Output ->error = "沒有上傳檔案";
     }
     else{
-        for ($i = 0; $i < $fileCount; $i++) {
-            if ($_FILES['my_file']['error'][$i] === UPLOAD_ERR_OK) # 檢查檔案是否上傳成功
-            {
-                $ext = explode('.', basename($_FILES['my_file']['name'][$i])); //将文件名按 “.” 分割成数组
-                $MD5_FILE_nAME =  md5(uniqid()) . "." . array_pop($ext);
-                if (file_exists('../upload/' . $MD5_FILE_nAME))  # 檢查檔案是否已經存在
+        if($Tile == null || $Introduce == null || $Sell_Price == null){$Fasle_Output ->error = "每項資料都要填寫";}
+        else{
+            for ($i = 0; $i < $fileCount; $i++) {
+                if ($_FILES['my_file']['error'][$i] === UPLOAD_ERR_OK) # 檢查檔案是否上傳成功
                 {
-                    $success = false;
-                    $Fasle_Output ->error = "上傳失敗,檔案已存在";
-                }
-                else {
-                    $file = $_FILES['my_file']['tmp_name'][$i];
-                    $dest = '../upload/' . $MD5_FILE_nAME;
-                    $File_Path = './upload/' . $MD5_FILE_nAME;
-                    move_uploaded_file($file, $dest); # 將檔案移至指定位置
-                    $Insert_Sql_Info = "INSERT INTO `check_sell_item`(`email`,`title`, `description`, `file_path`, `price`) VALUES ('$Username','$Tile','$Introduce','$File_Path','$Sell_Price')";
-                    if (mysqli_query($link, $Insert_Sql_Info)) {
-                        $success = true;
-                        $Success_Output ->success = "上傳成功";
+                    $ext = explode('.', basename($_FILES['my_file']['name'][$i])); //将文件名按 “.” 分割成数组
+                    $MD5_FILE_nAME =  md5(uniqid()) . "." . array_pop($ext);
+                    if (file_exists('../upload/' . $MD5_FILE_nAME))  # 檢查檔案是否已經存在
+                    {
+                        $success = false;
+                        $Fasle_Output ->error = "上傳失敗,檔案已存在";
                     }
                     else {
-                        $success = false;
-                        $Fasle_Output ->error = "連接資料庫失敗";
+                        $file = $_FILES['my_file']['tmp_name'][$i];
+                        $dest = '../upload/' . $MD5_FILE_nAME;
+                        $File_Path = './upload/' . $MD5_FILE_nAME;
+                        move_uploaded_file($file, $dest); # 將檔案移至指定位置
+                        $Insert_Sql_Info = "INSERT INTO `check_sell_item`(`email`,`title`, `description`, `file_path`, `price`) VALUES ('$Username','$Tile','$Introduce','$File_Path','$Sell_Price')";
+                        if (mysqli_query($link, $Insert_Sql_Info)) {
+                            $success = true;
+                            $Success_Output ->success = "上傳成功";
+                        }
+                        else {
+                            $success = false;
+                            $Fasle_Output ->error = "連接資料庫失敗";
+                        }
                     }
                 }
-            }
-            else {
-                $Fasle_Output ->error = "沒有上傳檔案";
-                $success = false;
+                else {
+                    $Fasle_Output ->error = "沒有上傳檔案";
+                    $success = false;
+                }
             }
         }
     }
